4c5af5dc9538210a69b9e8ff7a6a61eb972ab0ae
Arnav Jain <jain10@arnav.jain.se>
2020-06-17 20:00:48 +0200

    [AviationHeraldBridge] Add new bridge

---
+++ bridges/AviationHeraldBridge.php
@@ -0,0 +1,69 @@
+<?PHP
+class AviationHeraldBridge extends BridgeAbstract {
+
+	const NAME		  = 'The Aviation Herald';
+	const URI		  = 'https://avherald.com';
+	const DESCRIPTION = 'Returns the latest articles';
+	const MAINTAINER  = 'ajain-93';
+
+	public function collectData(){
+
+		$html = getSimpleHTMLDOM(self::URI)
+			or returnServerError('Could not request list: ' . self::URI);
+
+        $limit = 10;
+
+		foreach($html->find('td#ad1cell.center_td table') as $element) {
+
+            if(count($this->items) > $limit) {
+            break;
+            }
+    
+            // Debug::log($element);
+            
+            $title_span = $element->find('span.headline_avherald', 0);
+            $is_article = strlen($title_span) > 0;
+            if (!$is_article) {
+                // $date_span = $element->find('span.bheadline_avherald', 0);
+                // $date = $date_span->plaintext;
+
+                // Debug::log($date);
+                continue;
+            } 
+            $title = $title_span->plaintext;
+			$link = $element->find('a', 0);
+			$url = self::URI . $link->href;
+
+            // Debug::log($element);
+            // Debug::log("Article: " . $title);
+            // Debug::log("Link: " . $link);
+            // Debug::log("URL: " . $url);
+            
+            $article_html = getSimpleHTMLDOMCached($url, 1800 )
+			or returnServerError('Could not request article: ' . self::URI);
+            
+            $metadata = $article_html->find('.time_avherald', 0)->plaintext;
+            $last_updated_start = strpos($metadata, "last updated");
+            if ($last_updated_start > 0) {
+                $time_start = $last_updated_start + 13;
+            } else {
+                $time_start = strpos($metadata, "created") + 8;
+            }
+            $datetime = substr($metadata, $time_start);
+            
+            $content_all = $article_html->find('td#ad1cell.center_td', 0);
+            $content = $content_all->find('span.sitetext', 0);
+            // Debug::log($content);
+            
+
+			$item = array();
+			$item['uri'] = $url;
+			$item['title'] = $title;
+			$item['author'] = 'Simon Hradecky';
+			$item['timestamp'] = $datetime;
+			$item['content'] = trim($content);
+			$this->items[] = $item;
+            // break;
+		}
+	}
+}
