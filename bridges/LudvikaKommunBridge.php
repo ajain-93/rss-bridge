022a796d77bf4ded83a1a3469ff87f615ddef7e9
Arnav Jain <github@arnav.jain.se>
2022-11-05 10:40:43 +0100

    [LudvikaKommunBridge] Inital commit

---
+++ bridges/LudvikaKommunBridge.php
@@ -0,0 +1,84 @@
+<?PHP
+class LudvikaKommunBridge extends BridgeAbstract {
+
+	const NAME			= 'Ludvika Kommun';
+	const URI			= 'https://www.ludvika.se';
+	const DESCRIPTION	= 'News from Municipality of Ludvika';
+	const MAINTAINER	= 'ajain-93';
+	const NEWSPAGE		= '/nyheterochservicemeddelanden/nyheter.4.7c82b0fc1638b8db71b12ce9.html';
+	const AUTHOR		= 'Ludvika Kommun';
+	const LIMIT			= 20;
+
+	public function getIcon(){
+		return self::URI . '/images/18.421f7e0816662b0123c5f7c7/1542292999480/favicon-32x32.png';
+	}
+
+	public function collectData(){
+
+		$NEWSURL = self::URI . self::NEWSPAGE;
+
+
+		$html = getSimpleHTMLDOM($NEWSURL)
+			or returnServerError('Could not request: ' . $NEWSURL);
+
+		foreach($html->find('li.sv-channel-item') as $element) {
+
+			// Debug::log($element);
+
+			$link = $element->find('a', 0);
+			$url = self::URI . $link->href;
+			$title = $element->find('h2', 0)->plaintext;
+			$intro = '';
+			$text = '';
+			$datetime = strtotime($element->find('time', 0)->datetime);
+
+			// Debug::log($link);
+			// Debug::log($url);
+			// Debug::log($title);
+			// Debug::log($datetime);
+
+			$article_html = getSimpleHTMLDOMCached($url, 1800)
+			or returnServerError('Could not request article: ' . $url);
+
+			$content_div = $article_html->find('.pagecontent.sv-layout', 1);
+			// Debug::log($content_div);
+
+			foreach($content_div->find('div.sv-text-portlet.sv-use-margins') as $div) {
+
+				// Debug::log($div);
+
+				$isTitle = strlen($div->find('div#Rubrik', 0)) > 0;
+				if ($isTitle) {
+					$title = $div->find('div.sv-text-portlet-content', 0)->plaintext;
+					// Debug::log($title);
+				}
+
+				$isIntro = strlen($div->find('div#Ingress', 0)) > 0;
+				if ($isIntro) {
+					$intro = $div->find('div.sv-text-portlet-content', 0);
+					// Debug::log($intro);
+				}
+
+				$isText = strlen($div->find('div#Innehall', 0)) > 0;
+				if ($isText) {
+					$text = $div->find('div.sv-text-portlet-content', 0);
+					// Debug::log($text);
+				}
+			}
+
+			$item = array();
+			$item['uri'] = $url;
+			$item['title'] = $title;
+			$item['author'] = self::AUTHOR;
+			$item['timestamp'] = $datetime;
+			$item['content'] = trim("<i>" . $intro . "</i>" . $text);
+			$this->items[] = $item;
+
+			$limit = $this->getInput('limit') ?: -1;
+
+			if (count($this->items) > self::LIMIT) {
+				break;
+			}
+		}
+	}
+}
