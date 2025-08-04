ffe815bbbe68e9b2332599b37d7ecab2e3a130a3
Arnav Jain <git@arnav.jain.se>
2025-08-04 12:52:53 +0200

    [RegionStockholmBridge] Comment debug statements

--- bridges/RegionStockholmBridge.php
+++ bridges/RegionStockholmBridge.php
@@ -12,41 +12,41 @@ class RegionStockholmBridge extends BridgeAbstract {
         $html = getSimpleHTMLDOMCached(self::URI . '/Nyheter')
         or returnServerError('Could not request list: ' . self::URI);
 
-        Debug::log('LogTestStart');
+        // Debug::log('LogTestStart');
 
         foreach($html->find('article') as $element) {
         // foreach($html->find('div.filter-list-item') as $element) {
 
             $title_span = $element->find('h2', 0)->find('a', 0);
-            Debug::log('Title Span: ' . $title_span);
+            // Debug::log('Title Span: ' . $title_span);
 
             $title = $title_span->plaintext;
-            Debug::log('Title: ' . $title);
+            // Debug::log('Title: ' . $title);
 
             $url = 	$title_span->href;
-            Debug::log('URL: ' . $url);
+            // Debug::log('URL: ' . $url);
 
             $date = $element->find('time', 0)->datetime;
-            Debug::log('Date: ' . $date);
+            // Debug::log('Date: ' . $date);
 
             $category = $element->find('div div span',0)->plaintext;
-            Debug::log('Category: ' . $category);
+            // Debug::log('Category: ' . $category);
 
             $preamble = $element->find('p', 0)->plaintext;
-            Debug::log('Preamble: ' . $preamble);
+            // Debug::log('Preamble: ' . $preamble);
 
             $article_html = getSimpleHTMLDOMCached($url, 18000)
                 or returnServerError('Could not request article: ' . self::URI);
 
             $article = $article_html->find('div.prose', 0);
             $article_text = $article->find('div', 2);
-            Debug::log('Text: ' . $article_text);
+            // Debug::log('Text: ' . $article_text);
 
             $figure = $article_html->find('img.object-cover', 0)->src;
             $figure = str_replace('/_next/image/?url=', '', $figure);
             $figure = urldecode($figure);
             $figure = substr($figure, 0, strpos($figure, '&'));
-            Debug::log('Figure: ' . $figure);
+            // Debug::log('Figure: ' . $figure);
 
             if ($figure == null) {
                 $content = "<i>{$preamble}</i><br/><br/> {$article_text}";
@@ -54,7 +54,7 @@ class RegionStockholmBridge extends BridgeAbstract {
                 $content = "<i>{$preamble}</i><br/><img src=\"{$figure}\" /><br/><br/> {$article_text}";
                 // $content = "<i>{$preamble}</i><br/><img src=\"{$cover_image}\" /><br/>{$cover_caption}<br/> {$article_text}";
             }
-            Debug::log('Content: ' . $content);
+            // Debug::log('Content: ' . $content);
 
             $item = array();
             $item['uri'] = $url;
